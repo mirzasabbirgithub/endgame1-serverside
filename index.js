10f78204604c2cf6fb98174c64507bcd6c88e49c
mirzasabbirgithub <mirzaasabbir@gmail.com>
2022-07-01 23:41:52 +0600

    edited

--- index.js
+++ index.js
@@ -6,7 +6,20 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+const corsConfig = {
+          origin: '*',
+          credentials: true,
+          methods: ['GET', 'POST', 'PUT', 'DELETE']
+}
+app.use(cors(corsConfig))
+app.options("*", cors(corsConfig))
+
+app.use(function (req, res, next) {
+          res.header("Access-Control-Allow-Origin", "*")
+          res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,authorization")
+          next()
+})
 app.use(express.json());
 
 
